ed3b61695c0d4c8a270fade46c8343cb612c5b59
natoen <hgmatsuda@gmail.com>
2016-08-11 23:56:49 +0900

    (feat) Implement VideoPlayer component

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,13 +1,16 @@
-var VideoPlayer = () => (
+var VideoPlayer = ({video}) => (
+  video ?
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{video.snippet.title}</h3>
+      <div>{video.snippet.description}</div>
     </div>
   </div>
+  :
+  <div className="video-player">Loading...</div>
 );
 
 // PropTypes tell other developers what `props` a component expects
