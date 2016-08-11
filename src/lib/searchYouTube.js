5cff71a0603b99069188910d9e9a83eb1e1c9642
natoen <hgmatsuda@gmail.com>
2016-08-11 23:54:26 +0900

    (feat) Implement search Youtube function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,17 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+var searchYouTube = ({key, query}, callback) => {
+  $.get('https://www.googleapis.com/youtube/v3/search', {
+    part: 'snippet',
+    key: key,
+    q: query,
+    maxResults: 5,
+    type: 'video',
+    videoEmbeddable: 'true'
+  })
+  .done(({items}) => {
+    if (callback) {
+      callback(items);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
