212b1226c97a79bd292084ba912022f801c2cb68
natoen <hgmatsuda@gmail.com>
2016-08-12 18:41:22 +0900

    (refactor) Delete unnecessary comments

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -13,12 +13,8 @@ var VideoPlayer = ({video}) => (
   <div className="video-player">Loading...</div>
 );
 
-// PropTypes tell other developers what `props` a component expects
-// Warnings will be shown in the console when the defined rules are violated
 VideoPlayer.propTypes = {
   video: React.PropTypes.object.isRequired
 };
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
 window.VideoPlayer = VideoPlayer;
