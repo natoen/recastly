a10f5cc8b5cbd9828f81db2cd45d4778efb736d9
natoen <hgmatsuda@gmail.com>
2016-08-12 18:40:32 +0900

    (feat) Implement persistent search value

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,27 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+class Search extends React.Component { 
+  constructor(props) {
+    super(props);
 
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
+    this.state = {
+      value: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className="search-bar form-inline">
+        <input
+          className="form-control"
+          type="text"
+          onChange={event => { this.props.search(event.target.value); this.setState({ value: event.target.value }); }}
+          value={this.state.value}
+        />
+        <button className="btn hidden-sm-down">
+          <span className="glyphicon glyphicon-search"></span>
+        </button>
+      </div> 
+    );
+  }
+}
+
 window.Search = Search;
